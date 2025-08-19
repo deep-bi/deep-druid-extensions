0a27418252c4c64cbc2a716f08f24a22dbfb90c8
Vivek Dhiman <approach2vivek@gmail.com>
2025-08-18 21:18:38 -0700

    Addressed review comments

--- druid-filtering-emitter/src/main/java/bi/deep/filtering/EventFilter.java
+++ druid-filtering-emitter/src/main/java/bi/deep/filtering/EventFilter.java
@@ -17,7 +17,9 @@ package bi.deep.filtering;
 
 import bi.deep.FilteringEmitterConfig;
 import bi.deep.filtering.steps.*;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
